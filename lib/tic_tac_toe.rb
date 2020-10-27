2b1b3f4bc210bc3c90b70f7f2ec0fc06b1ec744c
Ashley Laswell <lswl9898@gmail.com>
2020-10-27 14:41:38 -0400

    add board class to lib

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,7 @@
 require_relative "tic_tac_toe/version"
 require_relative "./tic_tac_toe/cell.rb"
 require_relative "./tic_tac_toe/player.rb"
+require_relative "./tic_tac_toe/board.rb"
 
 module TicTacToe
 end
