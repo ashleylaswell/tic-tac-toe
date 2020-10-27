890e23d40af562b7ba96c87bed5b102c17595268
Ashley Laswell <lswl9898@gmail.com>
2020-10-27 17:50:03 -0400

    changed eq to truthy and falsey

--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -3,35 +3,35 @@ require "spec_helper"
 describe Array do
 	context "#all_empty?" do
 		it "returns true if all elements of the Array are empty" do
-			expect(["", "", ""].all_empty?).to eq true
+			expect(["", "", ""].all_empty?).to be_truthy
 		end
 		it "returns false if some of the Array elements are not empty" do
-			expect(["", 1, "", Object.new, :a].all_empty?).to eq false
+			expect(["", 1, "", Object.new, :a].all_empty?).to be_falsey
 		end
 		it "returns true for an empty Array" do
-			expect([].all_empty?).to eq true
+			expect([].all_empty?).to be_truthy
 		end
 	end
 	context "#all_same?" do
 		it "returns true if all elements of the Array are the same" do
-			expect(["A", "A", "A"].all_same?).to eq true
+			expect(["A", "A", "A"].all_same?).to be_truthy
 		end
 		it "returns false if some of the Array elements are not the same" do
-			expect(["", "1", "", Object.new, :a].all_same?).to eq false
+			expect(["", "1", "", Object.new, :a].all_same?).to be_falsey
 		end
 		it "returns true for an empty Array" do	
-			expect([].all_same?).to eq true
+			expect([].all_same?).to be_truthy
 		end
 	end
 	context "#any_empty?" do
 		it "returns true if any of the elements of the Array are empty" do
-			expect(["A", "A", ""].any_empty?).to eq true
+			expect(["A", "A", ""].any_empty?).to be_truthy
 		end
 		it "returns false if there are no empty elements in the Array" do
-			expect(["A", "A", "A"].any_empty?).to eq false
+			expect(["A", "A", "A"].any_empty?).to be_falsey
 		end
 		it "returns true for an empty Array" do
-			expect([].any_empty?).to eq true
+			expect([].any_empty?).to be_truthy
 		end
 	end
 end
